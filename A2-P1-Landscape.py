b950c9a6545e7963196d526e4864c02c860663c5
kylepreston24 <W0443556@campus.nscc.ca>
2024-10-26 05:22:51 -0300

    resubmit with name and student # up top A2 P1

--- A2-P1-Landscape.py
+++ A2-P1-Landscape.py
@@ -1,8 +1,8 @@
 #Program 1 – Landscape Calculator
 #A landscaping company needs a program that computes the price of landscaping for a new housing development. 
 
-#Student #:     
-#Student Name:  
+#Student #:    W0443556 
+#Student Name:  Kyle Preston
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
